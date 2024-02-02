e9dcfde4dc0a2cbc66b17bc7547fa6cfeadfed9e
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-02 16:05:35 -0300

    Retornando listas de objetos em JSON no spring Framework e Spring Boot

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package br.com.erudio.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +19,14 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id)throws Exception{
-		return service.findById(id);
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return service.findAll();
 	}
 	
-	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id){
+		return service.findById(id);
+	}
 	
 }
