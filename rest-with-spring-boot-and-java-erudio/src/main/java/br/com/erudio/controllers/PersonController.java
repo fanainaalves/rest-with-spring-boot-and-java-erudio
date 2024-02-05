27e1178bf71355041aa85f6d81dbe0cd497b39a5
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-05 16:48:27 -0300

    Inserindo a Seção de migrations

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
