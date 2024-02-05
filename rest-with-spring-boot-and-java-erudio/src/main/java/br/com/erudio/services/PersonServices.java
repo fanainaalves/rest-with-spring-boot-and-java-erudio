cc7a1939fb64dddb98da7726cf91a1ac62a1154b
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-05 12:46:30 -0300

    Subindo alterações da seção 8 (contém erros ao criar a tabela dno banco cod: Acess denied)

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,9 +1,7 @@
 package br.com.erudio.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.*;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,41 +10,42 @@ import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
-
 @Service
 public class PersonServices {
-
-	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-
 	
-	//Injeção de dependências
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
 	@Autowired
 	PersonRepository repository;
-	
-	
+
 	public List<Person> findAll() {
-		logger.info("Finding all people");
-	
-		
+
+		logger.info("Finding all people!");
+
 		return repository.findAll();
 	}
 
 	public Person findById(Long id) {
-		logger.info("Finding one person");
 		
-		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		logger.info("Finding one person!");
+		
+		return repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 	}
 	
 	public Person create(Person person) {
-		logger.info("Creating one person");
+
+		logger.info("Creating one person!");
 		
 		return repository.save(person);
 	}
-
+	
 	public Person update(Person person) {
-		logger.info("Update one person");
 		
-		var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		logger.info("Updating one person!");
+		
+		var entity = repository.findById(person.getId())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
@@ -57,10 +56,11 @@ public class PersonServices {
 	}
 	
 	public void delete(Long id) {
-		logger.info("Delete one person");
 		
-		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		logger.info("Deleting one person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		repository.delete(entity);
 	}
-	
 }
