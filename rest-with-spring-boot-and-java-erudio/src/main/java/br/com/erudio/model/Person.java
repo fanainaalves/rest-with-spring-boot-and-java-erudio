7329acdff139fb30436b87b94f917dd3be636cf4
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-05 09:27:22 -0300

    Criando a interface de conexão ao BD

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -3,27 +3,37 @@ package br.com.erudio.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "first_name", nullable=false)
 	private String firstName;
+	
+	@Column(name = "last_name", nullable=false)
 	private String lastName;
+	
+	@Column(nullable=false, length = 100)
 	private String address;
+	
+	@Column(nullable=false, length = 6)
 	private String gender;
 	
-	public Person(Long id, String firstName, String lastName, String address, String gender) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.gender = gender;
-	}
 
-	public Person() {
-		
-	}
+	public Person() {}
 
 	public Long getId() {
 		return id;
