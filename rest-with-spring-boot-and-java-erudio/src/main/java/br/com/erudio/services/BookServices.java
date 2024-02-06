06ab5dea73cefae605028022e1052e6933a8e293
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-06 17:26:37 -0300

    Alterações feitas durante aulas na seção 15

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/BookServices.java
@@ -0,0 +1,79 @@
+package br.com.erudio.services;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.controllers.PersonController;
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.exceptions.RequiredObjectIsNullException;
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.model.Book;
+import br.com.erudio.repositories.BookRepository;
+
+@Service
+public class BookServices {
+	
+	private Logger logger = Logger.getLogger(BookServices.class.getName());
+	
+	@Autowired
+	BookRepository repository;
+
+	public List<BookVO> findAll() {
+
+		logger.info("Finding all books!");
+		var persons = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
+		persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		
+		return persons;
+	}
+
+	public BookVO findById(Long id) {
+		
+		logger.info("Finding one book!");
+		
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		BookVO vo = DozerMapper.parseObject(entity, BookVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
+	}
+	
+	public BookVO create(BookVO book) {
+		if(book == null) throw new RequiredObjectIsNullException();
+		logger.info("Creating one book!");
+		var entity = DozerMapper.parseObject(book, Book.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	
+	public BookVO update(BookVO book) {
+		if(book == null) throw new RequiredObjectIsNullException();
+		logger.info("Updating one boook!");
+		
+		var entity = repository.findById(book.getKey()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunchDate(book.getLaunchDate());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deleting one book!");
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfig.java
@@ -0,0 +1,6 @@
+package br.com.erudio.configs;
+
+public class TestConfig {
+
+	public static final int SERVER_PORT = 8888;
+}
