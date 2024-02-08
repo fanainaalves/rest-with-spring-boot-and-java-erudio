b4dd90e6619a72956e7f667ad6a2253db870f36b
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-08 17:32:47 -0300

    corrigindo o nome length

--- src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
+++ src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
@@ -28,7 +28,7 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key:secret}")
     private String secretKey = "secret";
 
-    @Value("${security.jwt.token.expire-lenght:3600000}")
+    @Value("${security.jwt.token.expire-length:3600000}")
     private long validityInMilliseconds = 3600000;
 
     @Autowired
