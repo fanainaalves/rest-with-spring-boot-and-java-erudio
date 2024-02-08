4ca289ffa6792a2107cb227c1d1271224b257847
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-08 15:45:10 -0300

    Inserindo as migrations da seção 17, adicionando o spring security, JWT e Mapeando as entidades. E por fim, implementando o UserRepository e o UserService

---
+++ src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.erudio.repositories;
+
+import br.com.erudio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User WHERE u.userName =:userName")
+    User findByUserName(@Param("userName") String userName);
+}
