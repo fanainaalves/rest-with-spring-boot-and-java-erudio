6882b5332faa138f47213a5710610a53b2870101
Fanaina Alves <fanaiina.allves@gmail.com>
2024-02-09 14:37:53 -0300

    implementando as classes SecurityConfig, AuthServices, AuthController, application.yml(os 2)

--- src/main/java/br/com/erudio/repositories/UserRepository.java
+++ src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User WHERE u.userName =:userName")
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
     User findByUserName(@Param("userName") String userName);
 
 }
